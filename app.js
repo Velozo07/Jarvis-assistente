a428db1da829ba2a42fc066e982f0aaf4f5cf5b9
Velozo07 <dhigokarol@gmail.com>
2026-06-07 16:08:16 -0400

    Remove API key and GEMINI_URL from app.js
    Removed API key declaration and related URL.

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// Chave da API integrada
-const API_KEY = ""; 
-const GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + API_KEY;
-
 const statusTxt = document.getElementById('status');
 const transcricaoTxt = document.getElementById('transcricao');
 const respostaTxt = document.getElementById('resposta');
